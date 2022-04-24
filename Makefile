f311273424dfa056ec0bedeb948edd04f88255db
byunjuneseok <byunjuneseok@gmail.com>
2022-04-24 21:50:39 +0900

    Add "hello, world." function

--- Makefile
+++ Makefile
@@ -2,10 +2,14 @@
 
 zip: build
 	rm -f terraform/lambda.zip
-	zip terraform/lambda src/handler
+	cd src/ && zip ../terraform/lambda handler
 	rm src/handler
 
 
 build: src/main.go
 	cd src/ && go get .
 	cd src/ && GOOS=linux GOARCH=amd64 go build -o handler
+
+
+deploy: terraform/lambda.zip
+	cd terraform && terraform apply
--- src/main.go
+++ src/main.go
@@ -8,16 +8,10 @@ import (
 )
 
 func handleRequest(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-
-	queryParams, res := GetQueryParameters(req)
-	if res == nil {
-		return *res, nil
-	}
-
 	return events.APIGatewayProxyResponse{
 		StatusCode: http.StatusOK,
 		Headers:    map[string]string{"Content-Type": "text/plain; charset=utf-8"},
-		Body:       fmt.Sprintf("QueryParams. Skip: %d, Limit %d", queryParams.skip, queryParams.limit),
+		Body:       fmt.Sprintf("Hello, World."),
 	}, nil
 }
 
