fa237a351adcbe84ea02e453206d3e57aab4b65c
EC2 Default User <ec2-user@ip-172-31-28-74.ap-northeast-2.compute.internal>
2023-03-16 06:18:26 +0000

    edit

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,12 +2,11 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "4.11.0"
+      version = "4.53.0"
     }
   }
 }
 
 provider "aws" {
-  profile = var.aws_profile
   region  = var.aws_region
 }
