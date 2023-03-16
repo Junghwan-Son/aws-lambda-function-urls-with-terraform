0fcff65a402472d33762c50357cb638548251233
EC2 Default User <ec2-user@ip-172-31-28-74.ap-northeast-2.compute.internal>
2023-03-16 08:14:27 +0000

    edit

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,11 +1,11 @@
 resource "aws_lambda_function" "lambda" {
   function_name = var.function_name
   filename      = "lambda.zip"
-  handler       = "handler"
+  handler       = "index.handler"
   role          = aws_iam_role.lambda.arn
 
   source_code_hash = filebase64sha256("lambda.zip")
-  runtime = "go1.x"
+  runtime = "nodejs18.x"
 
   environment {
     variables = {
